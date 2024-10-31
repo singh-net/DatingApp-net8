3b1eceea11281367df50d951ee5f9979b82f8bec
Sandeep Singh <sam63156@hotmail.com>
2024-10-31 11:16:54 -0700

    JWT

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -7,16 +7,17 @@ using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    public class AccountController(DataContext context) : BaseApiController
+    public class AccountController(DataContext context, ITokenService tokenService) : BaseApiController
     {
         [HttpPost("register")] //account/register
 
-        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
 
             if (await UserExists(registerDto.Username)) return BadRequest("Username is already taken.");
@@ -33,12 +34,16 @@ namespace API.Controllers
 
             context.Users.Add(user);
             await context.SaveChangesAsync();
-            return user;
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = tokenService.CreateToken(user)
+            };
 
         }
 
         [HttpPost("login")]
-        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
             var user = await context.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
             if (user == null) return Unauthorized("Invalid username");
@@ -49,7 +54,11 @@ namespace API.Controllers
                 if (ComputedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
             }
 
-            return user;
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = tokenService.CreateToken(user)
+            };
 
         }
 
--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -4,15 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    
+
     public class UsersController(DataContext context) : BaseApiController
     {
 
+
+        [AllowAnonymous]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
@@ -22,6 +25,7 @@ namespace API.Controllers
 
         }
 
+        [Authorize]
         [HttpGet("{id:int}")] // /api/users/3
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
