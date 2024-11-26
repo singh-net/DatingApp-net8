a1f6ab37bb4d43cc2c1dab971b493ef8ec1adf2f
Sandeep Singh <sam63156@hotmail.com>
2024-11-26 11:54:09 -0800

    Updating Profile

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
@@ -15,13 +16,13 @@ namespace API.Controllers
 {
 
     [Authorize]
-    public class UsersController(IUserRepository userRepository) : BaseApiController
+    public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
     {
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
-            var users = await userRepository.GetMembersAsync();            
+            var users = await userRepository.GetMembersAsync();
             return Ok(users);
 
         }
@@ -34,6 +35,23 @@ namespace API.Controllers
             if (user == null) return NotFound();
             return user;
 
+        }
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (username == null) return BadRequest("No username found in token");
+
+            var user = await userRepository.GetUserByUserameAsync(username);
+
+            if (user == null) return BadRequest("Could not find user");
+
+            mapper.Map(memberUpdateDto, user);
+
+            if (await userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update the user");
         }
 
 
--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,30 +1,23 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { MembersService } from '../../_services/members.service';
 import { Member } from '../../_models/member';
 import { MemberCardComponent } from "../member-card/member-card.component";
 
 @Component({
-  selector: 'app-member-list',
-  standalone: true,
-  imports: [MemberCardComponent],
-  templateUrl: './member-list.component.html',
-  styleUrl: './member-list.component.css'
+    selector: 'app-member-list',
+    standalone: true,
+    templateUrl: './member-list.component.html',
+    styleUrl: './member-list.component.css',
+    imports: [MemberCardComponent]
 })
 export class MemberListComponent implements OnInit {
-
-  private memberService = inject(MembersService);
-  members: Member[] = [];
+  memberService = inject(MembersService);
 
   ngOnInit(): void {
-   this.loadMembers();
+    if (this.memberService.members().length === 0) this.loadMembers();
   }
-loadMembers() {
-  this.memberService.getMembers().subscribe({
-    next: members => this.members = members
 
-
-  })
-}
-
-
+  loadMembers() {
+    this.memberService.getMembers()
+  }
 }
